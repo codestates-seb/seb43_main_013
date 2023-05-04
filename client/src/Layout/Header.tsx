c0f16134aef9c5c070f08446fb917831c09ae91e
1-blue <1-blue98@naver.com>
2023-05-04 17:55:34 +0900

    [FE] design: header, nav 높이 및 main color 지정

--- client/src/Layout/Header.tsx
+++ client/src/Layout/Header.tsx
@@ -1,6 +1,6 @@
 /** 2023/05/04 - 헤더 컴포넌트 - by 1-blue */
 const Header = () => {
-  return <header></header>;
+  return <header className="bg-white h-[96px]"></header>;
 };
 
 export default Header;
--- client/src/Layout/Nav.tsx
+++ client/src/Layout/Nav.tsx
@@ -1,6 +1,6 @@
 /** 2023/05/04 - 네브 컴포넌트 - by 1-blue */
 const Nav = () => {
-  return <nav></nav>;
+  return <nav className="bg-white h-[64px] shadow-xl"></nav>;
 };
 
 export default Nav;
