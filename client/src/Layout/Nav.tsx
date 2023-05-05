227c00a28342543977caf1e6150cdcba80a0d28c
Kadesti <adesti3527@gmail.com>
2023-05-05 21:00:49 +0900

    [fe] feat: Header / Nav - DOM 구현

--- client/src/Layout/Nav.tsx
+++ client/src/Layout/Nav.tsx
@@ -1,6 +1,21 @@
-/** 2023/05/04 - 네브 컴포넌트 - by 1-blue */
+/** 2023/05/04 - 네브 컴포넌트 - by Kadesti */
 const Nav = () => {
-  return <nav className="bg-white h-[64px] shadow-xl"></nav>;
+  return (
+    <nav className="bg-white h-[64px] shadow-xl flex justify-center">
+      <div className="flex w-full max-w-5xl justify-between items-center">
+        <div className="flex text-xl justify-between w-80">
+          <div className="cursor-pointer text-black hover:text-rose-400">자유</div>
+          <div className="cursor-pointer text-black hover:text-rose-400">피드백</div>
+          <div className="cursor-pointer text-black hover:text-rose-400">홍보</div>
+          <div className="cursor-pointer text-black hover:text-rose-400">구인</div>
+        </div>
+        <div className="flex w-20 justify-between mr-12 cursor-default">
+          <div>1</div>
+          <div className="text-black hover:text-rose-400 cursor-pointer">해방일지</div>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
--- client/src/Layout/index.tsx
+++ client/src/Layout/index.tsx
@@ -4,7 +4,7 @@ import Nav from "./Nav";
 import Main from "./Main";
 import Footer from "./Footer";
 
-/** 2023/03/23 - 레이아웃을 적용하는 컴포넌트 - by 1-blue */
+/** 2023/05/04 - 레이아웃을 적용하는 컴포넌트 - by 1-blue */
 const Layout: React.FC<React.PropsWithChildren> = ({ children }) => (
   <>
     <Header />
