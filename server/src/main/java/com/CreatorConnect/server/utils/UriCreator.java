cf57c955cdc2dd2c6667b4d6c2690f2202b1028a
leemiyeon <lemingul7@gmail.com>
2023-05-06 16:55:31 +0900

    [be] feat: utils 기능 추가

---
+++ server/src/main/java/com/CreatorConnect/server/utils/UriCreator.java
@@ -0,0 +1,14 @@
+package com.CreatorConnect.server.utils;
+
+import org.springframework.web.util.UriComponentsBuilder;
+import java.net.URI;
+
+public class UriCreator {
+    public static URI createUri(String defaultUrl, long resourceId) {
+        return UriComponentsBuilder
+                .newInstance()
+                .path(defaultUrl + "/{resource-id}")
+                .buildAndExpand(resourceId)
+                .toUri();
+    }
+}
