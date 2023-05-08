12cbbe83da0279bd83340b6863dd1d9722193a42
seunghyun711 <seunghyun711@gmail.com>
2023-05-09 00:33:58 +0900

    [be] feat: FreeBoardDto 추가

---
+++ server/src/main/java/com/CreatorConnect/server/freeboard/dto/FreeBoardDto.java
@@ -0,0 +1,73 @@
+package com.CreatorConnect.server.freeboard.dto;
+
+import com.CreatorConnect.server.category.entity.Category;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+
+public class FreeBoardDto {
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Getter
+    @Setter
+    public static class Post {
+        @Valid
+        @NotBlank
+        private long memberId; // 작성자 id
+
+        @Valid
+        @NotBlank(message = "게시글의 제목을 입력하세요.")
+        private String title; // 게시글 제목
+
+        @Valid
+        @NotBlank(message = "내용을 작성하세요.")
+        private String content;
+
+        @Valid
+        @NotBlank(message = "카테고리를 선택학세요.")
+        private Category category;
+
+        // 태그 추가 예정
+        public void addMemberId(long memberId) {
+            this.memberId = memberId;
+        }
+    }
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Getter
+    @Setter
+    public static class Patch{
+        @Valid
+        private String title;
+
+        @Valid
+        private String content;
+    }
+
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Getter
+    @Setter
+    public static class Response{
+        @Positive
+        private long freeboardId;
+
+        private String title;
+
+        private String content;
+
+        private Category category;
+
+//        private long memberId;
+
+        // 카테고리 추가 예정
+
+        // 태그 추가 예정
+    }
+}
+
