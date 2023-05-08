f51f8f457175bc17fda8c9a6f51ffb6790514536
leemiyeon <lemingul7@gmail.com>
2023-05-09 05:06:29 +0900

    [be] fix: spring security 수정

--- server/src/main/java/com/CreatorConnect/server/auth/userdetails/MemberDetailsService.java
+++ server/src/main/java/com/CreatorConnect/server/auth/userdetails/MemberDetailsService.java
@@ -10,7 +10,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
-
 import java.util.Collection;
 import java.util.Optional;
 
