603ae3b752010a0a820e835452f6c60feeeefaf0
seunghyun711 <seunghyun711@gmail.com>
2023-05-08 21:56:57 +0900

    [be] feat: category 등록 기능 구현

---
+++ server/src/main/java/com/CreatorConnect/server/category/entity/Category.java
@@ -0,0 +1,25 @@
+package com.CreatorConnect.server.category.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long categoryId;
+
+    @Column(nullable = false)
+    private String category; // 카테고리 이름
+
+//    // category - freeboard 일대다 양방향 매핑
+//    @OneToMany(mappedBy = "category")
+//    private List<FreeBoard> freeBoards = new ArrayList<>();
+
+}
--- server/src/main/java/com/CreatorConnect/server/exception/ExceptionCode.java
+++ server/src/main/java/com/CreatorConnect/server/exception/ExceptionCode.java
@@ -7,7 +7,8 @@ public enum ExceptionCode {
     MEMBER_NOT_ALLOWED(403, "Access denied. Please authenticate as a member."),
     MEMBER_NOT_FOUND(404, "Member not found. Please check the member ID and try again."),
     MEMBER_EXISTS(409, "Member already exists. Please provide a different member ID."),
-    MEMBER_FIELD_NOT_FOUND(500, "Member field not found. Please check the object and try again.");
+    MEMBER_FIELD_NOT_FOUND(500, "Member field not found. Please check the object and try again."),
+    CATEGORY_EXISTS(409, "Category already exists. Please provide a different category");
 
     @Getter
     private int status;
