66638da4e2c5d620c8e462267a555b98aaa24d85
leemiyeon <lemingul7@gmail.com>
2023-05-09 13:52:02 +0900

    [be] fix: oauth google redirect url 수정

--- server/src/main/java/com/CreatorConnect/server/auth/oauth/google/GoogleOauthController.java
+++ server/src/main/java/com/CreatorConnect/server/auth/oauth/google/GoogleOauthController.java
@@ -1,13 +1,22 @@
 package com.CreatorConnect.server.auth.oauth.google;
 
+import com.CreatorConnect.server.member.dto.MemberResponseDto;
+import com.CreatorConnect.server.member.entity.Member;
+import com.CreatorConnect.server.member.mapper.MemberMapper;
+import com.CreatorConnect.server.member.repository.MemberRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class GoogleOauthController {
+
     private final OAuth2AuthorizedClientService authorizedClientService;
 
     public GoogleOauthController(OAuth2AuthorizedClientService authorizedClientService) {
@@ -16,16 +25,17 @@ public class GoogleOauthController {
 
     // http://localhost:8080/oauth2/authorization/google
     @GetMapping("/api/oauth/google")
-    public String oauth (Authentication authentication) {
+    public ResponseEntity oauthGoogle (Authentication authentication) {
+
         var authorizedClient = authorizedClientService.loadAuthorizedClient("google", authentication.getName());
 
         OAuth2AccessToken accessToken = authorizedClient.getAccessToken();
-        System.out.println("Access Token Value: " + accessToken.getTokenValue());
-        System.out.println("Access Token Type: " + accessToken.getTokenType().getValue());
-        System.out.println("Access Token Scopes: " + accessToken.getScopes());
-        System.out.println("Access Token Issued At: " + accessToken.getIssuedAt());
-        System.out.println("Access Token Expires At: " + accessToken.getExpiresAt());
+        log.info("Access Token Value: " + accessToken.getTokenValue());
+        log.info("Access Token Type: " + accessToken.getTokenType().getValue());
+        log.info("Access Token Scopes: " + accessToken.getScopes());
+        log.info("Access Token Issued At: " + accessToken.getIssuedAt());
+        log.info("Access Token Expires At: " + accessToken.getExpiresAt());
 
-        return "Access Token Value : " + accessToken.getTokenValue();
+        return new ResponseEntity<>("oauth google re-direct url", HttpStatus.OK);
     }
 }
