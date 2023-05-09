e5966a81c0ef3abd544fddf71b0aecf0f1673511
seunghyun711 <seunghyun711@gmail.com>
2023-05-09 23:50:12 +0900

    [be] feat: 페이지네이션 관련 DTO 추가

--- server/src/main/java/com/CreatorConnect/server/freeboard/dto/FreeBoardDto.java
+++ server/src/main/java/com/CreatorConnect/server/freeboard/dto/FreeBoardDto.java
@@ -6,11 +6,14 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.domain.Page;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
+import java.time.LocalDateTime;
+import java.util.List;
 
 public class FreeBoardDto {
     @AllArgsConstructor
@@ -72,14 +75,50 @@ public class FreeBoardDto {
 
         private String content;
 
+        private long commentCount;
+
+        private long likeCount;
+
+        private long viewCount;
+
         // 현재 카테고리 이름을 받아서 id 추출해서 저장하면 Category 테이블에 카테고리가 중복 저장되는 이슈로 인해 request로 categoryId를 받도록 임시조치
         private String categoryName;
 
-//        private long memberId;
-
-        // 카테고리 추가 예정
-
         // 태그 추가 예정
+
+        private LocalDateTime createdAt;
+
+        private LocalDateTime modifiedAt;
+
+        private long memberId;
+
+        private String email;
+
+        private String nickname;
+    }
+
+    // 페이지네이션 관련 dto
+    @Getter
+    @NoArgsConstructor
+    public static class MultiResponseDto<T> {
+        private List<T> data;
+        private PageInfo pageInfo;
+
+        public MultiResponseDto(List<T> data, Page page) {
+            this.data = data;
+            this.pageInfo = new PageInfo(page.getNumber() + 1,
+                    page.getSize(), page.getTotalElements(), page.getTotalPages());
+        }
+    }
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class PageInfo {
+        private int page;
+        private int size;
+        private long totalElements;
+        private int totalPages;
     }
 }
 
