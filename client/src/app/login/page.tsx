c41e9d2c12e8211f2a0b9c4eb7b0b44fd0c7e4be
Kadesti <adesti3527@gmail.com>
2023-05-09 21:04:26 +0900

    [fe] feat: 로그인 모달창

--- client/src/app/login/page.tsx
+++ client/src/app/login/page.tsx
@@ -1,19 +1,20 @@
 "use client";
 import { useState } from "react";
-import { Input, OAuthCon } from "../../components/login";
+import { Input, OAuthCon, SmallBtn } from "../../components/login";
 
 /** 2023/05/05 - 로그인 페이지 컴포넌트 - by Kadesti */
 const login = () => {
   const { idValid, pwValid } = useInputValid();
 
   return (
-    <div className="w-screen flex justify-center mt-16 px-auto">
+    <div className="w-screen flex justify-center my-16 px-auto">
       <div className="bg-white w-2/5 flex flex-col items-center p-6  rounded-xl drop-shadow-xl">
         <h1 className="text-5xl mb-6">로그인</h1>
         <form className="w-full">
           <Input label="아이디" valid={idValid} />
           <Input label="비밀번호" valid={pwValid} />
         </form>
+        <SmallBtn />
         <OAuthCon />
       </div>
     </div>
--- client/src/components/login.tsx
+++ client/src/components/login.tsx
@@ -1,9 +1,9 @@
 import Link from "next/link";
 
 /** 2023/05/05 - 로그인 인풋 창 - by Kadesti */
-const Input = ({ label, valid }: { label: string; valid: boolean }) => {
+const Input = ({ label, valid }: { label: "아이디" | "비밀번호"; valid: boolean }) => {
   return (
-    <button className="w-full h-28">
+    <div className="w-full h-28">
       <div className="text-xl flex mb-1">{label}</div>
       <input
         placeholder={`${label}`}
@@ -11,15 +11,21 @@ const Input = ({ label, valid }: { label: string; valid: boolean }) => {
         className="w-full h-12 border-2 border-black rounded-lg p-3 text-xl"
       />
       {valid && <div className="self-end text-rose-600">형식에 맞게 입력해주세요!</div>}
-    </button>
+    </div>
   );
 };
 
 /** 2023/05/05 - 회원가입 인풋 창 - by Kadesti */
-const SignInput = ({ label, valid }: { label: string; valid?: boolean | undefined }) => {
+const SignInput = ({
+  label,
+  valid,
+}: {
+  label: "아이디" | "비밀번호" | "닉네임" | "유튜브 주소" | "이메일" | "휴대폰 번호" | "자기소개";
+  valid?: boolean | undefined;
+}) => {
   return (
     <button className="w-full h-28">
-      <div className="text-xl mb-1">{label}</div>
+      <div className="flex text-xl mb-1">{label}</div>
       <input
         placeholder={`${label}`}
         type={label === "비밀번호" ? "password" : ""}
@@ -33,16 +39,16 @@ const SignInput = ({ label, valid }: { label: string; valid?: boolean | undefine
 /** 2023/05/05 - OAuth 로그인 버튼 - by Kadesti */
 const OAuthCon = () => {
   return (
-    <div className="w-4/5 mt-5">
+    <div className="w-4/5 mt-3">
       <button className="mb-6 bg-green-400 w-full h-16 flex justify-center items-center text-3xl rounded-2xl cursor-pointer hover:bg-green-200 hover:text-slate-400">
         로그인
       </button>
-      <Link
-        href="/signup"
-        className="mb-6 bg-green-400 w-full h-16 flex justify-center items-center text-3xl rounded-2xl cursor-pointer hover:bg-green-200 hover:text-slate-400"
-      >
-        회원가입
-      </Link>
+      <button className="mb-6 bg-green-400 w-full h-16 flex justify-center items-center text-3xl rounded-2xl cursor-pointer hover:bg-green-200 hover:text-slate-400">
+        OAuth
+      </button>
+      <button className="mb-6 bg-green-400 w-full h-16 flex justify-center items-center text-3xl rounded-2xl cursor-pointer hover:bg-green-200 hover:text-slate-400">
+        OAuth
+      </button>
       <button className="bg-green-400 w-full h-16 flex justify-center items-center text-3xl rounded-2xl cursor-pointer hover:bg-green-200 hover:text-slate-400">
         OAuth
       </button>
@@ -50,4 +56,23 @@ const OAuthCon = () => {
   );
 };
 
-export { Input, OAuthCon, SignInput };
+/** 2023/05/08 - 회원가입 비번 찾기 버튼 - by Kadesti */
+const SmallBtn = () => {
+  return (
+    <div className="flex self-end">
+      <Link
+        href="/signup"
+        className="mb-6 bg-green-400 p-1 mr-2 flex justify-center items-center text-lg rounded-lg cursor-pointer hover:bg-green-200 hover:text-slate-400"
+      >
+        회원가입
+      </Link>
+      <Link
+        href="/signup"
+        className="mb-6 bg-green-400 p-1 flex justify-center items-center text-lg rounded-lg cursor-pointer hover:bg-green-200 hover:text-slate-400"
+      >
+        비밀번호 찾기
+      </Link>
+    </div>
+  );
+};
+export { Input, OAuthCon, SignInput, SmallBtn };
