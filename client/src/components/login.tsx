d1ae6bbb62748a4ec92cf5e95aa2406ca30d6113
Kadesti <adesti3527@gmail.com>
2023-05-10 00:21:08 +0900

    [fe] fix:로그인 유효성 및 매핑

--- client/src/components/login.tsx
+++ client/src/components/login.tsx
@@ -16,23 +16,17 @@ const Input = ({ label, valid }: { label: "아이디" | "비밀번호"; valid: b
 };
 
 /** 2023/05/05 - 회원가입 인풋 창 - by Kadesti */
-const SignInput = ({
-  label,
-  valid,
-}: {
-  label: "아이디" | "비밀번호" | "닉네임" | "유튜브 주소" | "이메일" | "휴대폰 번호" | "자기소개";
-  valid?: boolean | undefined;
-}) => {
+const SignInput = ({ label, valid, isSubmit }: { label: string; valid?: boolean; isSubmit?: boolean }) => {
   return (
-    <button className="w-full h-28">
+    <div className="w-full h-28">
       <div className="flex text-xl mb-1">{label}</div>
       <input
         placeholder={`${label}`}
         type={label === "비밀번호" ? "password" : ""}
         className="w-full h-12 border-2 border-black rounded-lg p-3 text-xl"
       />
-      {valid && <div className="self-end text-rose-600">형식에 맞게 입력해주세요!</div>}
-    </button>
+      {!valid && isSubmit && <div className="self-end text-rose-600">형식에 맞게 입력해주세요!</div>}
+    </div>
   );
 };
 
@@ -40,15 +34,16 @@ const SignInput = ({
 const OAuthCon = () => {
   return (
     <div className="w-4/5 mt-3">
-      <button className="mb-6 bg-green-400 w-full h-16 flex justify-center items-center text-3xl rounded-2xl cursor-pointer hover:bg-green-200 hover:text-slate-400">
-        로그인
-      </button>
-      <button className="mb-6 bg-green-400 w-full h-16 flex justify-center items-center text-3xl rounded-2xl cursor-pointer hover:bg-green-200 hover:text-slate-400">
-        OAuth
-      </button>
-      <button className="mb-6 bg-green-400 w-full h-16 flex justify-center items-center text-3xl rounded-2xl cursor-pointer hover:bg-green-200 hover:text-slate-400">
-        OAuth
-      </button>
+      {["로그인", "OAuth", "OAuth"].map((el, i) => {
+        return (
+          <button
+            key={i}
+            className="mb-6 bg-green-400 w-full h-16 flex justify-center items-center text-3xl rounded-2xl cursor-pointer hover:bg-green-200 hover:text-slate-400"
+          >
+            {el}
+          </button>
+        );
+      })}
       <button className="bg-green-400 w-full h-16 flex justify-center items-center text-3xl rounded-2xl cursor-pointer hover:bg-green-200 hover:text-slate-400">
         OAuth
       </button>
