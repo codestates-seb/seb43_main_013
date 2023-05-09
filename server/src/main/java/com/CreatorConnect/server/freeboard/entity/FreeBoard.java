b24c21b5d74376e55bac82e5d4fd10f2615eeed7
seunghyun711 <seunghyun711@gmail.com>
2023-05-09 09:07:33 +0900

    [be] feat: FreeBoard 작성 시간, 수정 시간 추가

--- server/src/main/java/com/CreatorConnect/server/freeboard/entity/FreeBoard.java
+++ server/src/main/java/com/CreatorConnect/server/freeboard/entity/FreeBoard.java
@@ -1,22 +1,24 @@
 package com.CreatorConnect.server.freeboard.entity;
 
-import com.CreatorConnect.server.member.Member;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import com.CreatorConnect.server.category.entity.Category;
+import com.CreatorConnect.server.member.entity.Member;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class FreeBoard {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long freeboardId;
+    private Long freeboardId;
 
     @Column(nullable = false)
     private String title; // 게시글 제목
@@ -26,16 +28,30 @@ public class FreeBoard {
 
     // 태그(추가 예정)
 
-    private long commentCount; // 댓글수
+    private Long commentCount = 0L; // 댓글수
 
-    private long likeCount; // 좋아요수
+    private Long likeCount = 0L; // 좋아요수
 
-    private long viewCount; // 조회수
+    private Long viewCount = 0L; // 조회수
+
+    @CreatedDate
+    @Column(updatable = false, name = "CREATED_AT")
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @LastModifiedDate
+    @Column(name = "MODIFIED_AT")
+    private LocalDateTime modifiedAt = LocalDateTime.now();
 
     // 카테고리 (추가 예정)
 
+    // FreeBoard - Member 다대일 매핑
     @ManyToOne
     @JoinColumn(name = "MEMBER_ID")
     private Member member;
+
+    // FreeBoard - Category 다대일 매핑
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "CATEGORY_ID")
+    private Category category;
 
 }
