439740ca95ede0a8f908842f742a893592df02cd
Kadesti <adesti3527@gmail.com>
2023-05-10 19:56:17 +0900

    [fe] feat: 회원가입 페이지 SSR 전환

--- client/src/app/login/page.tsx
+++ client/src/app/login/page.tsx
@@ -1,9 +1,8 @@
-"use client";
-import { useState } from "react";
+import useInputValid from "../../hooks/useInputValid";
 import { Input, OAuthCon, SmallBtn } from "../../components/login";
 
 /** 2023/05/05 - 로그인 페이지 컴포넌트 - by Kadesti */
-const login = () => {
+const Login = () => {
   const { idValid, pwValid } = useInputValid();
 
   return (
@@ -21,12 +20,4 @@ const login = () => {
   );
 };
 
-export default login;
-
-/** 2023/05/05 - 로그인 유효성 체크 상태값 - by Kadesti */
-const useInputValid = () => {
-  const [idValid, setIdValid] = useState(false);
-  const [pwValid, setPWValid] = useState(false);
-
-  return { idValid, pwValid };
-};
+export default Login;
