2c4a51e6ded32aa68f5141f9414d37366d83c10b
leekoby <abc2752@naver.com>
2023-05-10 15:13:08 +0900

    [fe] refactor: 자유게시판 코드 분할
    1. 자유게시판 메인 구성 코드 분할

--- client/src/app/free/contentitem.tsx
+++ client/src/app/free/contentitem.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import { BookmarkIcon as BookmarkIconUnchecked } from "@heroicons/react/24/outline";
 import defaultThumnail from "@/public/images/default-thumnail.jpg";
-import ContentFooter from "./contentfooter";
+import ContentFooter from "./ContentFooter";
 import TagItem from "./TagItem";
 
 interface ContentItemProps {
@@ -17,37 +17,39 @@ interface ContentItemProps {
   nickname: string;
 }
 
+/** 2023/05/08 - 자유게시판 메인 화면 게시글 - by leekoby */
 const ContentItem: React.FC<ContentItemProps> = (props) => {
   return (
     <>
       {/*  list container */}
       <div className="flex flex-col items-center justify-center mt-2 bg-white rounded-md shadow-md md:flex-row ">
         {/* List Item * */}
-        <div className="flex flex-col items-center w-full mx-5 my-5 bg-gray-100 rounded-md md:flex-row">
+        <div className="flex flex-col items-center w-full h-full p-5 bg-gray-100 rounded-md ">
           {/* Thumnail */}
-          <Image src={defaultThumnail} className="h-auto my-12 ml-6 border rounded-lg w-80 md:w-32 md:h-28" alt="" />
+          {/* <Image src={defaultThumnail} className="h-auto mx-1 my-1 border rounded-lg w-80 md:w-40" alt="" /> */}
           {/* right content */}
-          <div className="flex flex-col w-full gap-2 p-2 md:p-5">
+          <div className="flex justify-between w-full h-full">
             {/* content header */}
-            <div className="flex items-center justify-between">
-              {/* rightside title */}
-              <h1 className="text-xl font-bold text-left ">{props.title}</h1>
-              <BookmarkIconUnchecked className="w-5 h-5 text-black cursor-pointer " />
+            <div className="flex flex-col justify-between w-full h-full gap-2 p-2 md:p-3">
+              <div className="flex items-center justify-between ">
+                {/* rightside title */}
+                <h1 className="w-full text-xl font-bold text-left">{props.title}</h1>
+                <BookmarkIconUnchecked className="w-5 h-5 text-black cursor-pointer " />
+              </div>
+              {/* content body */}
+              <p className="flex-1 w-full h-full text-left break-all line-clamp-3">{props.content}</p>
+              {/* rightside tag */}
+              <div className="flex w-full gap-x-2">{props.tag && props.tag.map((item) => <TagItem tag={item} />)}</div>
             </div>
-
-            {/* content body */}
-            <p className="w-full text-left break-all line-clamp-3 ">{props.content}</p>
-            {/* rightside tag */}
-            <div className="flex gap-x-2">{props.tag && props.tag.map((item) => <TagItem tag={item} />)}</div>
-            <ContentFooter
-              position="main"
-              nickName={props.nickname}
-              viewCount={props.viewCount}
-              likeCount={props.likeCount}
-              commentCount={props.commentCount}
-              createdAt={props.createdAt}
-            />
           </div>
+          <ContentFooter
+            position="main"
+            nickName={props.nickname}
+            viewCount={props.viewCount}
+            likeCount={props.likeCount}
+            commentCount={props.commentCount}
+            createdAt={props.createdAt}
+          />
         </div>
       </div>
     </>
