5901ada1d5cc17cca15508b7d945084197ad4922
leemiyeon <lemingul7@gmail.com>
2023-05-11 12:22:36 +0900

    [be] fix: login response 에 memberId 추가

--- server/src/main/java/com/CreatorConnect/server/auth/handler/MemberAuthenticationSuccessHandler.java
+++ server/src/main/java/com/CreatorConnect/server/auth/handler/MemberAuthenticationSuccessHandler.java
@@ -1,16 +1,27 @@
 package com.CreatorConnect.server.auth.handler;
 
+import com.CreatorConnect.server.member.repository.MemberRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Collections;
 
 @Slf4j
 @Component
 public class MemberAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+
+    private final MemberRepository memberRepository;
+
+    public MemberAuthenticationSuccessHandler(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request,
                                         HttpServletResponse response,
@@ -18,5 +29,19 @@ public class MemberAuthenticationSuccessHandler implements AuthenticationSuccess
 
         // 인증 성공 후, 로그를 기록하거나 사용자 정보를 response로 전송하는 등의 추가 작업을 할 수 있다.
         log.info("# Authenticated successfully!");
+
+        // 인증된 사용자의 memberId 가져오기
+        String username = ((UserDetails)authentication.getPrincipal()).getUsername();
+
+        Long memberId = memberRepository.findByEmail(username).get().getMemberId();
+
+        // memberId를 JSON 형식으로 응답하기
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonResult = objectMapper.writeValueAsString(Collections.singletonMap("memberId", memberId));
+
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.getOutputStream().write(jsonResult.getBytes());
+        response.flushBuffer();
     }
 }
