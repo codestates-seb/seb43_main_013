d8b319bc02c271baa4af7c37a0e9870127cece40
1-blue <1-blue98@naver.com>
2023-05-12 10:41:26 +0900

    [fe] feat: 홍보 상세 게시판 로직 작성 ( + 더미데이터, 임시 서버 )
    홍보 게시판 CRUD 로직 작성
    1. 상세 게시글 로직 작성 ( https://main-project.gitbook.io/main-project/undefined-1/undefined/undefined-5 )
    2. 댓글 로직 작성 ( https://main-project.gitbook.io/main-project/undefined-1/undefined-4 )

---
+++ client/src/pages/api/promotionboard/[promotionBoardId]/comment/new.ts
@@ -0,0 +1,23 @@
+import { timer } from "@/libs";
+
+// type
+import type { NextApiHandler } from "next";
+import type { ApiCreateCommentResponse } from "@/types/api";
+
+/** 2023/05/11 - 댓글 CUD - by 1-blue */
+const handler: NextApiHandler<ApiCreateCommentResponse> = async (req, res) => {
+  timer(1000);
+
+  try {
+    // 댓글 생성
+    if (req.method === "POST") {
+      return res.status(201).json({ commentId: 1 });
+    }
+  } catch (error) {
+    console.error("/api/feedbackboard/[feedbackBoardId]/comment/new error >> ", error);
+
+    return res.status(500).end();
+  }
+};
+
+export default handler;
