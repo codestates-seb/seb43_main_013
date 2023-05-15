eb676fc53e0ec20813035f0a460b27a7fcce8a55
seunghyun711 <seunghyun711@gmail.com>
2023-05-15 10:26:13 +0900

    [be] refactor: TagBoardRepository -> TagToFreeBoardRepository 인터페이스 이름 변경

--- server/src/main/java/com/CreatorConnect/server/tag/repository/TagToFreeBoardRepository.java
+++ server/src/main/java/com/CreatorConnect/server/tag/repository/TagToFreeBoardRepository.java
@@ -1,16 +1,14 @@
 package com.CreatorConnect.server.tag.repository;
 
 import com.CreatorConnect.server.freeboard.entity.FreeBoard;
-import com.CreatorConnect.server.tag.entity.Tag;
 import com.CreatorConnect.server.tag.entity.TagToFreeBoard;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
-public interface TagBoardRepository extends JpaRepository<TagToFreeBoard, Long> {
+public interface TagToFreeBoardRepository extends JpaRepository<TagToFreeBoard, Long> {
 //    Optional<TagToFreeBoard> findByTagToFreeBoard(FreeBoard freeBoard, Tag tag);
 
     List<TagToFreeBoard> findByFreeBoard(FreeBoard freeBoard);
