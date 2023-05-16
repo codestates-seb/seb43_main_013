eaf9b53cbc56123a2cfb82fe8facf40312f4ab41
1-blue <1-blue98@naver.com>
2023-05-17 03:26:41 +0900

    [fe] feat: 에디터 뷰어 스타일 생성 및 적용

--- client/src/components/Board/BoardContent.tsx
+++ client/src/components/Board/BoardContent.tsx
@@ -4,7 +4,7 @@ interface Props extends Pick<FreeBoard, "content"> {}
 
 /** 2023/05/11 - 게시판 내용 컴포넌트 - by 1-blue */
 const BoardContent: React.FC<Props> = ({ content }) => {
-  return <section dangerouslySetInnerHTML={{ __html: content }} className="py-4" />;
+  return <section dangerouslySetInnerHTML={{ __html: content }} className="viewer py-4" />;
 };
 
 export default BoardContent;
