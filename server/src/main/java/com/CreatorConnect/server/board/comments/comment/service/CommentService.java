89895f8053916e25741ad27c310175bf134a3f05
leemiyeon <lemingul7@gmail.com>
2023-05-17 03:44:20 +0900

    [be] refactor: comment 로그인 유저 인증 토큰 방식으로 리팩토링

--- server/src/main/java/com/CreatorConnect/server/board/comments/comment/service/CommentService.java
+++ server/src/main/java/com/CreatorConnect/server/board/comments/comment/service/CommentService.java
@@ -5,9 +5,9 @@ import com.CreatorConnect.server.board.comments.comment.dto.CommentResponseDto;
 
 public interface CommentService {
     CommentResponseDto.CommentContent createComment(Long id, CommentDto.Post postDto);
-    CommentResponseDto.CommentContent updateComment(Long boardId, Long commentId, CommentDto.Patch patchDto);
+    CommentResponseDto.CommentContent updateComment(String token, Long boardId, Long commentId, CommentDto.Patch patchDto);
     CommentResponseDto.Details responseComment(Long boardId, Long commentId);
     CommentResponseDto.Multi<CommentResponseDto.Details> responseComments(Long boardId, int page, int size);
-    void deleteComment(Long boardId, Long commentId);
+    void deleteComment(String token, Long boardId, Long commentId);
 
 }
