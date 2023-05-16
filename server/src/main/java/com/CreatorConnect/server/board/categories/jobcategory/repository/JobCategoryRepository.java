04b62681aa10bfec423cfa9cc6bd05f2f06f430c
seunghyun711 <seunghyun711@gmail.com>
2023-05-17 02:35:09 +0900

    [be] feat: JobCategoryRepository - findByJobCategoryName 구현

--- server/src/main/java/com/CreatorConnect/server/board/categories/jobcategory/repository/JobCategoryRepository.java
+++ server/src/main/java/com/CreatorConnect/server/board/categories/jobcategory/repository/JobCategoryRepository.java
@@ -4,6 +4,10 @@ import com.CreatorConnect.server.board.categories.jobcategory.entity.JobCategory
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface JobCategoryRepository extends JpaRepository<JobCategory, Long> {
+    Optional<JobCategory> findByJobCategoryName(String jobCategoryName);
 }
+
