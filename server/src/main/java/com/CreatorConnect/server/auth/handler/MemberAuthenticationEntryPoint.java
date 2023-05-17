e394c06e92f10d2bfd4c8da03236afb62819cec6
leemiyeon <lemingul7@gmail.com>
2023-05-17 13:37:49 +0900

    [be] refactor: response package refactor

--- server/src/main/java/com/CreatorConnect/server/auth/handler/MemberAuthenticationEntryPoint.java
+++ server/src/main/java/com/CreatorConnect/server/auth/handler/MemberAuthenticationEntryPoint.java
@@ -1,6 +1,6 @@
 package com.CreatorConnect.server.auth.handler;
 
-import com.CreatorConnect.server.errorresponse.ErrorResponder;
+import com.CreatorConnect.server.response.ErrorResponder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
