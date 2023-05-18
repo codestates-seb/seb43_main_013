a0cb11db632806e66129354776cf17638c86b644
leekoby <abc2752@naver.com>
2023-05-19 00:29:18 +0900

    [fe] feat: 홍보 게시판 메인 화면 제작중

--- client/src/app/promotion/page.tsx
+++ client/src/app/promotion/page.tsx
@@ -1,5 +1,16 @@
+import PromotionMain from "./PromotionMain";
+
+// type
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "CC | 홍보 게시판",
+};
+
+/** 2023/05/17 - 홍보 게시판 메인 화면 페이지 - by leekoby */
+
 const Page = () => {
-  return <>홍보게시판</>;
+  return <PromotionMain />;
 };
 
 export default Page;
