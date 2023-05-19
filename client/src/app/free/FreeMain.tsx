f4ed5abae4ac4e1d8f5a4af4d2b7347c301d38ce
leekoby <abc2752@naver.com>
2023-05-20 03:19:52 +0900

    [fe] feat: 자유게시판 메인 화면 북마크, 좋아요 기능 추가

--- client/src/app/free/FreeMain.tsx
+++ client/src/app/free/FreeMain.tsx
@@ -1,6 +1,5 @@
 "use client";
-import Link from "next/link";
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { useFetchFreeBoardList } from "@/hooks/query/useFetchFreeBoardList";
 import { useFetchCategories } from "@/hooks/query";
 import SideCategories from "@/components/BoardMain/SideCategories";
@@ -40,9 +39,11 @@ const FreeMain = () => {
 
   /** 2023/05/13 - 공통 카테고리 초기값 - by leekoby */
   const { categories, isLoading } = useFetchCategories({ type: "normal" });
-
   if (!data) return <FullSpinner />;
   if (data.pages.length < 1) return <FullSpinner />;
+
+  // 북마크 좋아요 확인용 콘솔로그
+  // console.log(data.pages);
 
   return (
     //  전체 컨테이너
@@ -58,34 +59,35 @@ const FreeMain = () => {
           {/* <PopularPosts /> */}
         </aside>
         {/* rightside freeboard post list */}
-        <section className="flex flex-col md:w-0 ml-5  grow-[8]">
+        <section className="flex flex-col md:w-0 ml-5 grow-[8]">
           {/* freeboard list header */}
-          <div className="flex justify-end">
+          <div className="flex justify-end  mb-4">
             <SortPosts />
           </div>
 
           {/* post item */}
-          {/* TODO: //*게시글 북마크 좋아요 클릭되게 하는 방법 생각해보기  */}
-          {data.pages.map((item) =>
-            item.data.map((innerData) => (
-              <Link href={`/free/${innerData.freeBoardId}`}>
-                <ContentItem props={innerData} key={innerData.freeBoardId} />
-              </Link>
-            )),
-          )}
+          <div className="space-y-5">
+            {data.pages.map((item) =>
+              item.data.map((innerData) => (
+                <div className="" key={innerData.freeBoardId}>
+                  <ContentItem props={innerData} />
+                </div>
+              )),
+            )}
 
-          {/* postslist bottom */}
-          <div className="flex justify-center items-center">
-            {/* TODO React Query를 이용한 PreFetch 방식으로 변경하기 */}
-            <Pagination
-              page={data?.pages[0].pageInfo.page}
-              totalPages={data?.pages[0].pageInfo.totalPages}
-              onPageChange={setCurrentPage}
-            />
+            {/* postslist bottom */}
+            <div className="flex justify-center items-center">
+              {/* TODO React Query를 이용한 PreFetch 방식으로 변경하기 */}
+              <Pagination
+                page={data?.pages[0].pageInfo.page}
+                totalPages={data?.pages[0].pageInfo.totalPages}
+                onPageChange={setCurrentPage}
+              />
+            </div>
           </div>
         </section>
         {/* 오른쪽 사이드 영역 */}
-        <div className="flex flex-col items-center justify-center ml-2">
+        <div className="opacity-50 lg:opacity-100 fixed right-0 lg:top-1/2 transform -translate-y-1/2 ml-2">
           <RightSideButton destination={`/free/write`} />
         </div>
       </div>
