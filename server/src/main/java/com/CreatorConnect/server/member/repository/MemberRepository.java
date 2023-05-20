3442ac253110787351c0caa9e56b67448783ce40
leemiyeon <lemingul7@gmail.com>
2023-05-20 13:12:37 +0900

    [be] refactor: 회원가입 오류 메세지 수정

--- server/src/main/java/com/CreatorConnect/server/member/repository/MemberRepository.java
+++ server/src/main/java/com/CreatorConnect/server/member/repository/MemberRepository.java
@@ -8,5 +8,7 @@ import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmail(String email);
+    Optional<Member> findByNickname(String nickname);
+    Optional<Member> findByPhone(String phone);
     Page<Member> findByNicknameContaining(String keyword, Pageable pageable);
 }
