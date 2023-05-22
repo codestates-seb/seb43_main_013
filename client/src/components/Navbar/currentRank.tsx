acb803c764d8c42a0a1611b78b3b102b085df75a
leekoby <abc2752@naver.com>
2023-05-22 22:18:32 +0900

    [fe] bug: 네트워크 요청 버그 주석처리

--- client/src/components/Navbar/currentRank.tsx
+++ client/src/components/Navbar/currentRank.tsx
@@ -5,17 +5,15 @@ import { useState } from "react";
 const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 
 type rank = () => string[];
-const currentRank: rank = () => {
-  const [curRank, setCurRank] = useState([""]);
-
-  const currentRank = async () => {
-    const rankArr = await axios.get(`${baseUrl}/api/search/popularkeywords`);
-    return rankArr.data.data;
-  };
-
-  currentRank().then((res) => setCurRank(res));
-
-  return curRank;
+// const currentRank: rank = () => {
+const currentRank = () => {
+  //   const [curRank, setCurRank] = useState([""]);
+  //   const currentRank = async () => {
+  //     const rankArr = await axios.get(`${baseUrl}/api/search/popularkeywords`);
+  //     return rankArr.data.data;
+  //   };
+  //   currentRank().then((res) => setCurRank(res));
+  //   return curRank;
 };
 
 export default currentRank;
