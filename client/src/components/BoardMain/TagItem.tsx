00aafbd665d452ff1bab95941de9fddced73409c
leekoby <abc2752@naver.com>
2023-05-24 02:27:00 +0900

    [fe] design: 게시판 게시글 목록 내 태그 크기 조정

--- client/src/components/BoardMain/TagItem.tsx
+++ client/src/components/BoardMain/TagItem.tsx
@@ -9,7 +9,7 @@ const TagItem: React.FC<TagItemProps> = ({ tag }) => {
     <li className="list-none">
       <button
         type="button"
-        className="px-3 py-1 bg-main-400 text-sm text-white rounded-lg transition-colors hover:font-bold hover:bg-main-500"
+        className="px-2 py-1 bg-main-400 text-xs text-white rounded-lg transition-colors hover:font-bold hover:bg-main-500"
       >
         {tag}
       </button>
