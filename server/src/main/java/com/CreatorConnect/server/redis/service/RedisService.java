6db029095527c7306a941f8f3f80e7607ac7bbff
seunghyun711 <seunghyun711@gmail.com>
2023-05-24 00:28:08 +0900

    [be] feat: RedisService - Access Token redis에 저장 및 조회 기능 추가

--- server/src/main/java/com/CreatorConnect/server/redis/service/RedisService.java
+++ server/src/main/java/com/CreatorConnect/server/redis/service/RedisService.java
@@ -7,6 +7,7 @@ import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 @Service
 @Slf4j
@@ -27,5 +28,23 @@ public class RedisService {
     public String getRefreshToken(String refreshToken) {
         ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
         return valueOperations.get(refreshToken);
+    }
+
+    // AccessToken  redis 저장(key : accessToken, value : "logout")
+    public void setAccessTokenLogOut(String accessToken, int expirationMinutes) {
+        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        valueOperations.set(accessToken, "logout", expirationMinutes);
+        log.info("Access Token 만료 시간 : {} ", Duration.ofMinutes(expirationMinutes));
+    }
+
+    // redis에 저장된 access token 조회
+    public String getAccessToken(String accessToken) {
+        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        return valueOperations.get(accessToken);
+    }
+
+    // redis에 저장된 refresh token 삭제
+    public void deleteRefreshToken(String refreshToken) {
+        redisTemplate.delete(refreshToken);
     }
 }
