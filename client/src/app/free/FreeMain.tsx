31c29ed27888bce369408b0322346146535c5ad9
leekoby <abc2752@naver.com>
2023-05-24 10:53:46 +0900

    [fe] fix: 자유게시판  hydration-error 해결

--- client/src/app/free/FreeMain.tsx
+++ client/src/app/free/FreeMain.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useFetchFreeBoardList } from "@/hooks/query/useFetchFreeBoardList";
 import { useFetchCategories } from "@/hooks/query";
 import SideCategories from "@/components/BoardMain/SideCategories";
@@ -14,6 +14,12 @@ import NoDataExists from "@/components/Svg/NoDataExists";
 /** 2023/05/08 - 자유게시판 메인 화면 - by leekoby */
 const FreeMain = () => {
   const member = useMemberStore((state) => state.member);
+
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
 
   /** 2023/05/14 - 게시판 page 상태관리 - by leekoby */
   const currentPage = usePageStore((state) => state.currentPage);
@@ -47,9 +53,11 @@ const FreeMain = () => {
     //  전체 컨테이너
     <div className="mx-auto mt-6">
       <h2 className="text-2xl font-bold text-left"> 자유게시판 </h2>
-      <div className="flex justify-end  mb-4">
-        <SortPosts />
-      </div>
+      {isClient && (
+        <div className="flex justify-end  mb-4">
+          <SortPosts />
+        </div>
+      )}
       <div className="flex flex-col md:flex-row ">
         {/* Left Side */}
         {/* rightside freeboard post list */}
@@ -74,7 +82,6 @@ const FreeMain = () => {
 
             {data && (
               <div className="flex justify-center items-center">
-                {/* TODO: React Query를 이용한 PreFetch 방식으로 변경하기 */}
                 <Pagination
                   page={data?.pages[0].pageInfo.page}
                   totalPages={data?.pages[0].pageInfo.totalPages}
@@ -85,7 +92,7 @@ const FreeMain = () => {
           </div>
         </section>
         {/* 오른쪽 사이드 영역 */}
-        {member && <RightSideButton destination={`/free/write`} />}
+        {isClient && member && <RightSideButton destination={`/free/write`} />}
       </div>
     </div>
   );
