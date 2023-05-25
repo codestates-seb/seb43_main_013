ce0195862f4a3b17cf6adfec90ba737a9960f8fd
Kadesti <adesti3527@gmail.com>
2023-05-25 10:09:10 +0900

    [fe] bug: 헤더 로그인상태, 로그인 서버 문제

--- client/src/Layout/Header.tsx
+++ client/src/Layout/Header.tsx
@@ -14,10 +14,10 @@ import AuthCheck from "@/components/Header/AuthCheck";
 /** 2023/05/04 - 헤더 컴포넌트 - by Kadesti */
 const Header: React.FC = () => {
   const { accessToken } = useTokenStore();
-  const [hasToken, setHasToken] = useState(accessToken);
+  const [hasToken, setHasToken] = useState(!!accessToken);
 
   useEffect(() => {
-    setHasToken(accessToken);
+    setHasToken(!!accessToken);
   }, [accessToken]);
 
   const nickState = useState(false);
@@ -25,12 +25,12 @@ const Header: React.FC = () => {
 
   return (
     <header className="bg-white h-[96px] border-b-4 flex justify-center">
-      {/* <AuthCheck /> */}
+      <AuthCheck />
       <div className="flex w-full max-w-[1440px] items-center">
         <HeaderLogo />
         <SearchSide array={leftArr} />
         <SearchDiv />
-        {hasToken !== "" ? <LoginSide nickState={nickState} /> : <SearchSide array={rightArr} />}
+        {hasToken ? <LoginSide nickState={nickState} /> : <SearchSide array={rightArr} />}
       </div>
     </header>
   );
--- client/src/components/Header/AuthCheck.tsx
+++ client/src/components/Header/AuthCheck.tsx
@@ -6,6 +6,7 @@ import { useTokenStore } from "@/store/useTokenStore";
 import { useRouter } from "next/navigation";
 
 const AuthCheck = () => {
+  const router = useRouter();
   const { refreshToken, setAccessToken } = useTokenStore();
 
   serverInstance.interceptors.response.use(function (value) {
@@ -13,7 +14,7 @@ const AuthCheck = () => {
 
     // 2. 엑세스 토큰이 만료된 경우
     // 3. 둘 다 만료된 경우
-    // const refreshProcess = useCallback((router: any) => {
+    // const refreshProcess = (router: any) => {
     //   axios
     //     .post("/refresh-token", {
     //       headers: {
@@ -23,18 +24,24 @@ const AuthCheck = () => {
     //     .then((res) => setAccessToken(res.data))
     //     // 3. 둘 다 만료된 경우
     //     .catch(() => router.push("/login"));
-    // }, []);
+    // };
 
-    console.log("value: ", value);
+    // console.log("value: ", value);
+    // console.log("value.status: ", value.status);
+    // console.log("value.statusText: ", value.statusText);
+
+    // console.log("value.status: ", value.status);
 
     useEffect(() => {
-      const router = useRouter();
-
+      console.log("value: ", value);
       // 2. 엑세스 토큰이 만료된 경우
-      // if (value.status === 401 || value.statusText === "unauthenticated") {
-      //   refreshProcess(router);
-      // }
-    }, [value.status]);
+      if (value.status === 401 || value.statusText === "unauthenticated") {
+        // refreshProcess(router);
+      }
+      if (value.status === 200) console.log("실행");
+      // if (value.status === 200) console.log("value: ", value);
+    }, []);
+    // }, [value.status]);
 
     return value;
   });
