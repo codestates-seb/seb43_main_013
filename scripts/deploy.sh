f07d4e1bdf0be173be37a55e4290d2f746dda536
lim3873 <82255760+lim3873@users.noreply.github.com>
2023-05-25 10:55:48 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-BUILD_JAR=$(ls /home/ec2-user/action/server/build/libs/*.jar)
+BUILD_JAR=$(ls /home/ec2-user/action/build/libs/*.jar)
 JAR_NAME=$(basename $BUILD_JAR)
 echo "> build 파일명: $JAR_NAME" >> /home/ec2-user/action/deploy.log
 
