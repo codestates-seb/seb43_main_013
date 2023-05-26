8115a97f0a7854574ea445774ef8d4186b6648aa
seunghyun711 <seunghyun711@gmail.com>
2023-05-26 16:51:52 +0900

    [be] feat: RedisService - AccessToken 저장 / 조회 구현

---
+++ server/src/main/java/com/CreatorConnect/server/auth/redis/service/RedisService.java
@@ -0,0 +1,37 @@
+package com.CreatorConnect.server.auth.redis.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class RedisService {
+    private final RedisTemplate redisTemplate;
+
+    // AccessToken redis에 저장 (key : accessToken, value : "logout")
+    public void setAccessTokenLogOut(String accessToken, long expiration) {
+        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        valueOperations.set(accessToken, "logout", expiration, TimeUnit.MILLISECONDS);
+        String expirationTime = String.format("%d min, %d sec",
+                TimeUnit.MILLISECONDS.toMinutes(expiration),
+                TimeUnit.MILLISECONDS.toSeconds(expiration) -
+                        TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(expiration))
+        );
+        log.info("Access Token 만료 시간 : {}", expirationTime);
+
+    }
+
+    // Redis에 저장된 accessToken 조회
+    public String getAccessToken(String accessToken) {
+        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        return valueOperations.get(accessToken).replace("Bearer ", "");
+    }
+
+}
