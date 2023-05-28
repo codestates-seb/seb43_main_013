b42dedf7b2d23315dc6cdf4ce808c457e4560618
lim3873 <82255760+lim3873@users.noreply.github.com>
2023-05-29 06:38:16 +0900

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 PROJECT_ROOT="/home/ec2-user/app/deploy"
-JAR_FILE="$PROJECT_ROOT/server/build/libs/seb43_main_013.jar"
+JAR_FILE="$PROJECT_ROOT/server/build/libs/server-0.0.1-SNAPSHOT.jar"
 
 APP_LOG="$PROJECT_ROOT/application.log"
 ERROR_LOG="$PROJECT_ROOT/error.log"
