8b2d58d0b9376aeb75f94658cd2f4eeb7bbef942
lim3873 <lim3873@naver.com>
2023-05-29 11:13:39 +0900

    [be] fix: RedisConfig 수정 test

--- server/src/main/java/com/CreatorConnect/server/redis/RedisConfig.java
+++ server/src/main/java/com/CreatorConnect/server/redis/RedisConfig.java
@@ -2,8 +2,10 @@ package com.CreatorConnect.server.redis;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -11,7 +13,9 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+@PropertySource("classpath:application.yml")
 @Configuration
+@ConfigurationProperties(prefix = "spring.redis")
 @RequiredArgsConstructor
 @EnableRedisRepositories
 public class RedisConfig {
