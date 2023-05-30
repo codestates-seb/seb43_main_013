f521026b42857d92c6029b75ff34235b9bf20c70
leemiyeon <lemingul7@gmail.com>
2023-05-30 12:20:31 +0900

    [be] refactor: redis config

--- server/src/main/java/com/CreatorConnect/server/redis/RedisConfig.java
+++ server/src/main/java/com/CreatorConnect/server/redis/RedisConfig.java
@@ -1,37 +1,25 @@
-package com.CreatorConnect.server.redis;
+package com.CreatorConnect.server.auth.jwt.refreshToken;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-@PropertySource("classpath:application.yml")
 @Configuration
-@ConfigurationProperties(prefix = "spring.redis")
-@RequiredArgsConstructor
-@EnableRedisRepositories
 public class RedisConfig {
 
-//    private final String redisHost;
-//    private final int redisPort;
-//
-//    public RedisConfig(@Value("${spring.redis.host}") final String redisHost,
-//                       @Value("${spring.redis.port}") final int redisPort) {
-//        this.redisHost = redisHost;
-//        this.redisPort = redisPort;
-//    }
-    @Value("${spring.redis.host}")
-    private String redisHost;
-    @Value("${spring.redis.port}")
-    private int redisPort;
+    private final String redisHost;
+    private final int redisPort;
+
+    public RedisConfig(@Value("${spring.redis.host}") final String redisHost,
+                       @Value("${spring.redis.port}") final int redisPort) {
+        this.redisHost = redisHost;
+        this.redisPort = redisPort;
+    }
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
@@ -39,10 +27,8 @@ public class RedisConfig {
     }
 
     @Bean
-    public RedisTemplate<String, String> redisTemplate() {
-        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new StringRedisSerializer());
+    public RedisTemplate<?, ?> redisTemplate() {
+        RedisTemplate<?, ?> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory());
         return redisTemplate;
     }
